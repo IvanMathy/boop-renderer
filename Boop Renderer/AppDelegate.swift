7d22cbd81f714aacce26e6a52ac9e45cabe1edca
Ivan Mathy <IvanMathy@users.noreply.github.com>
2022-09-05 19:04:54 -0700

    Initial Commit

---
+++ Boop Renderer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Boop Renderer
+//
+//  Created by Ivan Mathy on 9/5/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
